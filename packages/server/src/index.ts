c39ff3a036e9afe413f1e53e17ccbbc489815010
Mashiro Wang <hyperoot@gmail.com>
2019-08-12 16:34:29 +0800

    perf: ⚡️ add koa-redis-cache

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -2,11 +2,22 @@ import * as Koa from 'koa'
 import * as Router from 'koa-router'
 import * as bodyParser from 'koa-bodyparser'
 import * as cors from '@koa/cors'
+import * as cache from 'koa-redis-cache'
 import AppRoutes from './routes'
 
 const app = new Koa()
 const router = new Router()
 const port = process.env.PORT || 8080
+const redisOptions = {
+  expire: 24 * 60 * 60,
+  redis: {
+    post: 6379,
+    host: '127.0.0.1',
+    options: {
+      password: '9Ef4cmr8jwaTs6cbYGJXVURsnbRAjLbAoh7LE6DsByGcCTuXt',
+    },
+  },
+}
 
 //路由
 AppRoutes.forEach(route => router[route.method](route.path, route.action))
@@ -15,6 +26,7 @@ app.use(cors())
 app.use(bodyParser())
 app.use(router.routes())
 app.use(router.allowedMethods())
+app.use(cache(redisOptions))
 app.listen(port)
 
 console.log(`应用启动成功 端口:${port}`)
